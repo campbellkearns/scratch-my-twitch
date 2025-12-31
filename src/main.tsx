18db8be03b4ce1ccd9f526f19234cc6d8dc1a0f9
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2025-12-31 18:35:06 -0500

    Adds splash and loading screens

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,20 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+// Hide the loading screen when React app is ready
+const hideLoadingScreen = () => {
+  const loadingElement = document.getElementById('app-loading');
+  if (loadingElement) {
+    document.body.classList.add('app-loaded');
+    // Remove loading screen after transition
+    setTimeout(() => {
+      if (loadingElement.parentNode) {
+        loadingElement.parentNode.removeChild(loadingElement);
+      }
+    }, 300);
+  }
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
@@ -11,3 +25,6 @@ createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </StrictMode>,
 )
+
+// Hide loading screen after React has rendered
+setTimeout(hideLoadingScreen, 100);
